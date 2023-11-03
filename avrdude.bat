668bfe7c881ccb716cef49fec83f460c80921db7
nmoore678 <124629549+nmoore678@users.noreply.github.com>
2023-11-03 09:57:44 -0400

    Add files via upload

---
+++ avrdude.bat
@@ -0,0 +1,6 @@
+
+mkdir %LOCALAPPDATA%\Arduino15
+mkdir %LOCALAPPDATA%\Arduino15\staging
+mkdir %LOCALAPPDATA%\Arduino15\staging\packages
+xcopy C:\avrdude\avrdude-6.3.0-arduino17-i686-w64-mingw32.zip %LOCALAPPDATA%\Arduino15\staging\packages
+
